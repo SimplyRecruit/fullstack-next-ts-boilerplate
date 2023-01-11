fe050936ce7da8841917c934eb90ed8ee133803f
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-01-11 12:29:37 +0300

    feat: Type checking for props with no validation

--- models/LoginReqBody.ts
+++ models/LoginReqBody.ts
@@ -1,8 +1,9 @@
-import { IsEmail, MinLength } from "class-validator";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
 export default class {
     @IsEmail()
     email: string;
 
+    @IsString()
     password: string;
 }
